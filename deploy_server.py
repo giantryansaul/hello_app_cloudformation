d4792941f240c671fee016fb564b66c2ec2399b9
Ryan Saul <seccom@gmail.com>
2017-02-08 23:50:34 -0800

    resolved template issues with rendering joins

--- deploy_server.py
+++ deploy_server.py
@@ -1,22 +1,36 @@
+from datetime import datetime
 
 import boto3
 from jinja2 import Template
 
+session = boto3.Session(profile_name='grs')
+client = boto3.client('cloudformation')
+
 
 def format_cf_join(multiline):
-    formatted_input = []
+    formatted_input = '[ '
     for line in multiline.splitlines():
-        formatted_input.append('\"{}\n\"'.format(line))
-    return formatted_input
-
-cloudformation_template = Template(open('cf.template').read())
-hello_app = open('hello_app.py').read()
-
-userdata_template = Template(open('userdata.sh').read())
-
-print(cloudformation_template.render(
-    hello_app=format_cf_join(hello_app),
-    userdata=format_cf_join(userdata_template.render()))
-)
+        formatted_input += ("\"{}\\n\" ,".format(line))
+    return formatted_input.rstrip(',') + ' ]'
 
 
+def create_cloudformation_template():
+
+    hello_app = open('hello_app.py').read()
+    userdata_template = Template(open('userdata.sh').read())
+    cloudformation_template = Template(open('cf.template').read())
+    return cloudformation_template.render(
+            hello_app=format_cf_join(hello_app),
+            userdata=format_cf_join(userdata_template.render())
+    )
+
+
+def main():
+    client.create_stack(
+        StackName='hello-app-{}'.format(datetime.today().strftime("%Y%m%d%H%M%S")),
+        TemplateBody=create_cloudformation_template()
+    )
+
+
+if __name__ == '__main__':
+    main()
--- hello_app.py
+++ hello_app.py
@@ -1,14 +1,15 @@
 # Hello World app
+# If you use double-quotes you're going to have a bad time!
 
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route("/hello")
+@app.route('/hello')
 def hello():
     return 'Hello World', 200
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(80))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(80))
